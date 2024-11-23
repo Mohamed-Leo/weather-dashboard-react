96a22b2e7d9a8921df9387e80b0c8c005d966c2b
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-11-23 20:33:24 +0200

    refactor: chnage the eslint.config.js file

--- eslint.config.js
+++ eslint.config.js
@@ -6,36 +6,79 @@ import tseslint from "typescript-eslint";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
-	{ ignores: ["dist"] },
+	{
+		ignores: ["dist"],
+	},
 	{
 		extends: [
 			js.configs.recommended,
 			...tseslint.configs.recommended,
-			eslintPluginPrettier, // Prettier handles formatting rules
+			eslintPluginPrettier,
 		],
 		files: ["**/*.{ts,tsx}"],
 		languageOptions: {
 			ecmaVersion: 2020,
-			globals: globals.browser,
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
 		},
 		plugins: {
 			"react-hooks": reactHooks,
 			"react-refresh": reactRefresh,
 		},
 		rules: {
+			// React Hooks rules
 			...reactHooks.configs.recommended.rules,
 			"react-refresh/only-export-components": [
 				"warn",
 				{ allowConstantExport: true },
 			],
-			"prettier/prettier": [
+
+			// TypeScript specific rules
+			"@typescript-eslint/no-explicit-any": "warn",
+			"@typescript-eslint/explicit-function-return-type": "off",
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{ argsIgnorePattern: "^_" },
+			],
+			"@typescript-eslint/consistent-type-imports": [
+				"error",
+				{ prefer: "type-imports" },
+			],
+
+			// General code quality rules
+			"no-console": ["warn", { allow: ["warn", "error"] }],
+			"no-debugger": "warn",
+			"no-duplicate-imports": "error",
+			"no-unused-expressions": "error",
+			"no-var": "error",
+			"prefer-const": "error",
+			"spaced-comment": ["error", "always"],
+
+			// Disable conflicting rules
+			indent: "off",
+			"no-tabs": "off",
+
+			// Import rules
+			"import/no-default-export": "off",
+			"import/order": [
 				"error",
 				{
-					useTabs: true, // Make sure ESLint follows Prettier's tab rule
+					groups: [
+						"builtin",
+						"external",
+						"internal",
+						["parent", "sibling"],
+						"index",
+					],
+					"newlines-between": "always",
+					alphabetize: {
+						order: "asc",
+						caseInsensitive: true,
+					},
 				},
 			],
-			indent: "off", // Disable ESLint's own indent rule
-			"no-tabs": "off", // Ensure ESLint does not complain about tabs
 		},
 	},
 );
