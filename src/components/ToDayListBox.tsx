f816d414ab00d0ee37df2375a97173641d5c33d1
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-12-18 23:33:49 +0200

    fix: fix the today forecast issue for showing message when no data

--- src/components/ToDayListBox.tsx
+++ src/components/ToDayListBox.tsx
@@ -8,7 +8,7 @@ import {
 	CarouselItem,
 } from "@/components/ui/carousel";
 import { IForecastItem, IWeatherForecast } from "@/interfaces";
-import getToDayForeCast from "@/utils/GetToDayForeCast";
+import getToDayForeCast from "@/utils/getToDayForeCast";
 
 interface IToDayBoxProps {
 	backGround: string;
@@ -16,31 +16,41 @@ interface IToDayBoxProps {
 }
 
 function ToDayListBox({ foreCastFivedays, backGround = "" }: IToDayBoxProps) {
-	// get today forecast by using the GetToDayForeCast utility---
+	// get today forecast by using the getToDayForeCast utility---
 	const toDayForeCast = getToDayForeCast(foreCastFivedays);
 
 	// plugin for Carousel----
 	const plugin = React.useRef(Autoplay({ delay: 3000 }));
 
+	// check for toDayForeCast data and return depending on the case---
 	return (
 		<div className={`today-list-boxes w-full ${backGround} p-5 rounded-3xl`}>
-			<Carousel
-				plugins={[plugin.current]}
-				className="w-full"
-				onMouseEnter={plugin.current.stop}
-				onMouseLeave={plugin.current.reset}
-			>
-				<CarouselContent>
-					{toDayForeCast.map((todayCast: IForecastItem) => (
-						<CarouselItem
-							key={todayCast.dt_txt}
-							className="basis-1/3 lg:basis-1/3 mx-auto"
-						>
-							<ToDayBox key={todayCast.dt_txt} todayForeCastData={todayCast} />
-						</CarouselItem>
-					))}
-				</CarouselContent>
-			</Carousel>
+			{toDayForeCast.length === 0 ? (
+				<h2 className="text-center capitalize w-full">
+					no today forecast for now
+				</h2>
+			) : (
+				<Carousel
+					plugins={[plugin.current]}
+					className="w-full"
+					onMouseEnter={plugin.current.stop}
+					onMouseLeave={plugin.current.reset}
+				>
+					<CarouselContent>
+						{toDayForeCast.map((todayCast: IForecastItem) => (
+							<CarouselItem
+								key={todayCast.dt_txt}
+								className="basis-1/3 lg:basis-1/3 mx-auto"
+							>
+								<ToDayBox
+									key={todayCast.dt_txt}
+									todayForeCastData={todayCast}
+								/>
+							</CarouselItem>
+						))}
+					</CarouselContent>
+				</Carousel>
+			)}
 		</div>
 	);
 }
